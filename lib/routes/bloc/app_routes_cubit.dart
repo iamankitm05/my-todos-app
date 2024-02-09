c214e639b9d3ca767f880d6278fbdf9952b5ab16
Ankit kumar <iamankitm05@gmail.com>
2024-02-09 10:34:06 +0530

    initial commit

--- lib/routes/bloc/app_routes_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_todos_app/routes/app_routes.dart';
-import 'package:my_todos_app/routes/bloc/app_routes_state.dart';
-
-class AppRoutesCubit extends Cubit<AppRoutesState> {
-  AppRoutesCubit() : super(AppRoutesState(routes: AppRoutes().router));
-}
--- lib/routes/routes_constant.dart
+++
@@ -1,4 +0,0 @@
-class RouteNameConstant {
-  static const String todosListing = 'todosListing';
-  static const String todoCreation = 'todoCreation';
-}
